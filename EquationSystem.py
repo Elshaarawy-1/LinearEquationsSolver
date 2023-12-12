1a4e2df707254bab0bb6b4e569cbcc7ef1c0217e
Ahmed Youssef <a.goerany@gmail.com>
2023-12-12 22:51:41 +0200

    add matrix b  (Ax=b)

--- EquationSystem.py
+++ EquationSystem.py
@@ -1,13 +1,20 @@
+# Ax=b
 class EquationSystem:
-    def __init__(self, coefficients, isLinear):
-        if (not self.validate(coefficients)):
+    def __init__(self, coefficients, constants, isLinear):
+        if (not self.validate(coefficients,constants)):
             return None
-        self.coefficients = coefficients # matrix
+        self.coefficients = coefficients # matrix A
+        self.constants = constants # matrix B
         self.is_linear=isLinear # boolean
     
-    def validate(coefficients):
+    def validate(coefficients,constants):
         for row in coefficients:
             for elem in row:
                 if (not elem.isdigit()) and (not elem.isalpha()): 
                     return False
+                
+        for elem in constants:
+            if (not elem.isdigit()) and (not elem.isalpha()): 
+                return False
+
         return True
