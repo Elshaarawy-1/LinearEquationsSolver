151c627eb39a7b7d705b381dfcec9f6b422ea538
MoustafaAmer12 <126791188+MoustafaAmer12@users.noreply.github.com>
2023-12-15 07:07:21 +0200

    Gauss-Seidel SF

--- GaussSeidelSolver.py
+++ GaussSeidelSolver.py
@@ -1,28 +1,33 @@
 import numpy as np
 from sympy import symbols, pprint, Add, Mul, Eq
+import mpmath
 import Solver
 
 class GaussSeidelSolver():
     def __init__(self, matrix, soln, itr=50,tol=1e-10,x=None, sf = 5):
-        self.matrix = matrix.astype(float)
-        self.soln = soln.astype(float)
+        self.matrix = matrix
+        self.soln = soln
         self.itr = itr
         self.tol = tol
-        self.x = x.astype(float)
+        self.x = x
         self.sf = sf
 
+
     def solve(self):
-        x_prev = self.x.copy()
-        row, col = self.matrix.shape
+        mpmath.mp.dps = self.sf
+        mpmath.mp.pretty = True
+        x_prev = mpmath.matrix(self.x)
+        row = self.matrix.rows
+        col = self.matrix.cols
         if row != col:
             raise ValueError("Matrix is not square")
 
         #Check diagonally dominant
         self.matrix, converge = self.diagonally_dominant(self.matrix, False)
-        diagonal = np.diag(self.matrix)
+        diagonal = np.diag(np.array(self.matrix.tolist()))
         
         print(f"convergence: {converge}")
-        print(f"Initial Guess: {self.x}")
+        print(f"Initial Guess:\n{self.x}")
         i = 0
         while True:
             i += 1
@@ -33,14 +38,14 @@ class GaussSeidelSolver():
                 self.x[j] = self.soln[j]
                 for k in range(row):
                     if k != j:
-                        self.x[j] -= (self.matrix[j,k]*self.x[k])
+                        self.x[j] -= (self.matrix[j,k] * self.x[k])
                         x_expr = x_expr - Mul(self.matrix[j,k],self.x[k])
                 self.x[j] /= diagonal[j]
                 x_expr = x_expr/diagonal[j]
                 x_expr = Eq(x_symb, x_expr)
                 pprint(x_expr)
 
-            print(f"Solution for iteration {i}: {self.x}\n")
+            print(f"Solution for iteration {i}: \n{self.x}\n")
             err = np.absolute(self.x - x_prev)/self.x * 100
             print(f"Error for Iteration {i}: {err}")
             x_prev = self.x.copy()
@@ -52,7 +57,7 @@ class GaussSeidelSolver():
 
 
     def diagonally_dominant(self, array, parsed_bef):
-        abs_matrix = np.absolute(array)
+        abs_matrix = np.absolute(array.tolist())
         diagonal = np.diag(abs_matrix)
         greater = False
         gr_eq = True
@@ -90,10 +95,10 @@ class GaussSeidelSolver():
     
 
 if __name__ == '__main__':
-    matrix = np.array([[12,3,-5],[1,5,3],[3,7,13]])
-    soln = np.array([1,28,76])
+    matrix = mpmath.matrix([[4,2,1],[-1,2,0],[2,1,4]])
+    soln = mpmath.matrix([11,3,16])
 
-    solver = GaussSeidelSolver(matrix, soln, itr=6,
-                               x=np.array([1,0,1]))
+    solver = GaussSeidelSolver(matrix, soln, itr=4,
+                               x=mpmath.matrix([1,1,1]), sf = 5)
     solution = solver.solve()
-    print("Final Solution: ", solution)
+    print("\nFinal Solution:\n",solution)
